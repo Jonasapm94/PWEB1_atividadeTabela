5c5f6c20b52aa33249ee9bc610752b0a1d9213a5
Jonas A <jonasapm94@gmail.com>
2023-06-01 23:13:38 -0300

    criação de filtro da tabela

--- src/app/usuario/listagem-usuario-tabela/listagem-usuario-tabela.component.ts
+++ src/app/usuario/listagem-usuario-tabela/listagem-usuario-tabela.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/shared/modelo/usuario';
 import { UsuarioService } from 'src/app/shared/services/usuario.service';
 
@@ -9,8 +10,9 @@ import { UsuarioService } from 'src/app/shared/services/usuario.service';
 })
 export class ListagemUsuarioTabelaComponent {
 
-  usuarios: Usuario[] = [];
+  usuarios: Usuario[]  = [];
   columnsToDisplay = ['nome','cpf', 'idade', 'acoes'];
+  dataSource = new MatTableDataSource(this.usuarios);
 
   constructor(private usuarioService: UsuarioService) {
   }
@@ -35,6 +37,12 @@ export class ListagemUsuarioTabelaComponent {
       );
     }
 
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log('chegou aqui')
   }
 
 }
